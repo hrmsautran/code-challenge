290a43e9abf8f2fade35e542dc07e379dbbf3ef1
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 14:17:04 -0400

    Add test to ensure method should return Statud Code 200

--- Tests/Unit/CalculaJurosAPI/Controllers/CalculaJurosControllerTest.cs
+++ Tests/Unit/CalculaJurosAPI/Controllers/CalculaJurosControllerTest.cs
@@ -1,11 +1,23 @@
 ﻿using Xunit;
 using FluentAssertions;
-using TaxaJurosAPI.Controllers;
+using CalculaJurosAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Tests.Unit.CalculaJurosAPI.Controllers
 {
     public class CalculaJurosControllerTest
     {
+        [Fact]
+        public void CalculaJuros_DeveRetornarStatusCode200()
+        {
+            // Arrange
+            var controller = new CalculaJurosController();
+
+            // Act
+            var result = (OkResult)controller.Get();
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+        }
     }
 }
