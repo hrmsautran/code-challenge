47cb4821bf88c882a7784a444355da1872a9a207
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 11:43:45 -0400

    Add TaxaDeJurosController

---
+++ TaxaDeJurosAPI/Controllers/TaxaDeJurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxaDeJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaDeJurosController : ControllerBase
+    {
+        [HttpGet(Name = "taxaJuros")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
