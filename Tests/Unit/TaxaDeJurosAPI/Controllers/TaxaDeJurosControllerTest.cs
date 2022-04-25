5342addd7123f82f232c3a2b41cdfcc4334af1b6
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 13:05:27 -0400

    Add test do ensure method should not return null

--- Tests/Unit/TaxaDeJurosAPI/Controllers/TaxaDeJurosControllerTest.cs
+++ Tests/Unit/TaxaDeJurosAPI/Controllers/TaxaDeJurosControllerTest.cs
@@ -8,16 +8,29 @@ namespace Tests.Unit.TaxaDeJurosAPI.Controllers
     public class TaxaDeJurosControllerTest
     {
         [Fact]
-        public void ObterTaxaDeJuros_DeveRetornarStatusCode200 ()
+        public void ObterTaxaDeJuros_DeveRetornarStatusCode200()
         {
             // Arrange
-            var controller = new TaxaDeJurosController ();
+            var controller = new TaxaDeJurosController();
 
             // Act
             var result = (OkObjectResult)controller.ObterTaxaDeJuros();
 
             // Assert
             result.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public void ObterTaxaDeJuros_NaoDeveRetornarNulo()
+        {
+            // Arrange
+            var controller = new TaxaDeJurosController();
+
+            // Act
+            var result = (OkObjectResult)controller.ObterTaxaDeJuros();
+
+            // Assert
+            result.StatusCode.Should().NotBeNull();
         }
     }
 }
