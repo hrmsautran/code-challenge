f9be3fcea4ffc25673b2849bef4870eafcaac67c
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 15:04:59 -0400

    Refact: change Math.Round to Math.Floor to ensure that result is exactaly original two decimal places

--- CalculaJurosAPI/Models/Juros.cs
+++ CalculaJurosAPI/Models/Juros.cs
@@ -13,11 +13,11 @@
         public int Meses { get; set; }
         public double TaxaDeJuros { get; set; }
 
-        protected decimal Calcular()
+        public decimal Calcular()
         {
             var resutl = this.ValorInicial * (decimal)Math.Pow(1 + this.TaxaDeJuros, this.Meses);
 
-            return Math.Round(resutl, 2);
+            return Math.Floor(resutl * 100) / 100M;
         }
     }
 }
