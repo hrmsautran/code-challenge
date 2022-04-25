60642f390fd94c683b4301c2b1fb12f7bd61df72
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 11:58:28 -0400

    Create test do ensure method return status code 200

--- Tests/Unit/TaxaDeJurosAPI/Controllers/TaxaDeJurosControllerTest.cs
+++ Tests/Unit/TaxaDeJurosAPI/Controllers/TaxaDeJurosControllerTest.cs
@@ -1,10 +1,23 @@
 ﻿using Xunit;
 using FluentAssertions;
 using TaxaDeJurosAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tests.Unit.TaxaDeJurosAPI.Controllers
 {
     public class TaxaDeJurosControllerTest
     {
+        [Fact]
+        public void ObterTaxaDeJuros_DeveRetornarStatusCode200 ()
+        {
+            // Arrange
+            var controller = new TaxaDeJurosController ();
+
+            // Act
+            var result = (OkObjectResult)controller.ObterTaxaDeJuros();
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+        }
     }
 }
