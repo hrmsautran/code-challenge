19feec6bf6aa81798f79d802ad8bbfa3d1a8a0bc
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 14:13:47 -0400

    Add CalculaJurosController Test

---
+++ Tests/Unit/CalculaJurosAPI/Controllers/CalculaJurosControllerTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using FluentAssertions;
+using TaxaJurosAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tests.Unit.CalculaJurosAPI.Controllers
+{
+    public class CalculaJurosControllerTest
+    {
+    }
+}
