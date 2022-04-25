7e5f87d9e6af8ad7900bdd55f6a85cced112f695
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 13:10:40 -0400

    Refact: change to return fixed fee

--- TaxaDeJurosAPI/Controllers/TaxaDeJurosController.cs
+++ TaxaDeJurosAPI/Controllers/TaxaDeJurosController.cs
@@ -6,10 +6,12 @@ namespace TaxaDeJurosAPI.Controllers
     [Route("[controller]")]
     public class TaxaDeJurosController : ControllerBase
     {
+        protected const double TAXA_DE_JUROS = 0.01;
+
         [HttpGet(Name = "taxaJuros")]
         public IActionResult ObterTaxaDeJuros()
         {
-            return Ok(1);
+            return Ok(TAXA_DE_JUROS);
         }
     }
 }
