7f6668c4f8d363c739c652d180a280aed7b5012b
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 14:47:41 -0400

    Create class Juros Model

---
+++ CalculaJurosAPI/Models/Juros.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculaJurosAPI.Models
+{
+    public class Juros
+    {
+        public Juros(decimal valorInicial, int meses, double taxaDeJuros)
+        {
+            ValorInicial = valorInicial;
+            Meses = meses;
+            TaxaDeJuros = taxaDeJuros;
+        }
+
+        public decimal ValorInicial { get; set; }
+        public int Meses { get; set; }
+        public double TaxaDeJuros { get; set; }
+
+        protected decimal Calcular()
+        {
+            var resutl = this.ValorInicial * (decimal)Math.Pow(1 + this.TaxaDeJuros, this.Meses);
+
+            return Math.Round(resutl, 2);
+        }
+    }
+}
