94ed1eaff16f8978c0285350f7373ad03e26651a
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 23:47:31 -0400

    Refact: changed test to mocked Service

--- Tests/Unit/CalculaJurosAPI/Controllers/CalculaJurosControllerTest.cs
+++ Tests/Unit/CalculaJurosAPI/Controllers/CalculaJurosControllerTest.cs
@@ -2,38 +2,34 @@
 using FluentAssertions;
 using CalculaJurosAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using CalculaJurosAPI.Services;
+using Moq;
+using System.Threading.Tasks;
 
 namespace Tests.Unit.CalculaJurosAPI.Controllers
 {
     public class CalculaJurosControllerTest
     {
-        [Fact]
-        public void CalculaJuros_DeveRetornarStatusCode200()
-        {
-            // Arrange
-            var controller = new CalculaJurosController();
-
-            // Act
-            var result = (OkObjectResult)controller.Get(100, 5);
-
-            // Assert
-            result.StatusCode.Should().Be(200);
-        }
-
         [Theory]
         [InlineData(100, 5, 105.10)]
-        [InlineData(90, 8, 97.45)]
-        [InlineData(100.40, 3, 103.44)]
-        public void CalcularJuros_DeveRetornarValorCalculado (decimal valorInicial, int meses, decimal expected)
+        public async Task CalcularJuros_DeveRetornarValorCalculado(decimal valorInicial, int meses, decimal expected)
         {
             // Arrange
-            var controller = new CalculaJurosController();
+            var mockService = new Mock<ITaxaDeJurosService>();
+
+            mockService
+                .Setup(service => service.GetAsync())
+                .ReturnsAsync(0.01);
+
+            var controller = new CalculaJurosController(mockService.Object);
 
             // Act
-            var result = (OkObjectResult)controller.Get(valorInicial, meses);
+            var contentResult = await controller.Get(valorInicial, meses);
+
+            var result = ((OkObjectResult)contentResult).Value;
 
             // Assert
-            result.Value.Should().Be(expected);
+            result.Should().Be(expected);
         }
     }
 }
