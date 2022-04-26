c1fdd52775c61803d616a056c4baf4c597d0b241
Hermes Autran <hermes.autran@gmail.com>
2022-04-25 22:37:30 -0400

    Refact: change to httpClientFactory

--- CalculaJurosAPI/Services/TaxaDeJurosService.cs
+++ CalculaJurosAPI/Services/TaxaDeJurosService.cs
@@ -4,30 +4,26 @@ namespace CalculaJurosAPI.Services
 {
     public class TaxaDeJurosService : ITaxaDeJurosService
     {
-        private readonly string _baseUrl;
-        private readonly HttpClient _client;
-        private readonly IConfiguration _configuration;
+        readonly IHttpClientFactory _httpClientFactory;
 
-        public TaxaDeJurosService(HttpClient client, IConfiguration configuration)
+        public TaxaDeJurosService(IHttpClientFactory httpClientFactory)
         {
-            this._client = client;
-            this._configuration = configuration;
-            this._baseUrl = _configuration.GetValue<string>("API_URL");
+            _httpClientFactory = httpClientFactory;
         }
 
         public async Task<decimal> GetAsync()
         {
-            var httpResponse = await _client.GetAsync(_baseUrl);
+            using var httpClient = _httpClientFactory.CreateClient("apiTaxaDeJuros");
 
-            if (!httpResponse.IsSuccessStatusCode)
+            var result = await httpClient.GetAsync("taxaJuros");
+
+            if (!result.IsSuccessStatusCode)
             {
-                throw new Exception("Não foi possível buscar a taxa de juros.");
+                throw new HttpRequestException("Não foi possível buscar a taxa de juros.");
             }
 
-            var content = await httpResponse.Content.ReadAsStringAsync();
-            var taxa = JsonConvert.DeserializeObject<decimal>(content);
-
-            return taxa;
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<decimal>(content);
         }
     }
 }
