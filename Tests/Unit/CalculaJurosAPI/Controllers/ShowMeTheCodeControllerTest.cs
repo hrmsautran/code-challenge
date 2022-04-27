11630a29c22097d2ed48742a419324f561dd8337
Hermes Autran <hermes.autran@gmail.com>
2022-04-27 16:09:02 -0400

    Implemented ShowMeTheCode controllerTest

---
+++ Tests/Unit/CalculaJurosAPI/Controllers/ShowMeTheCodeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using FluentAssertions;
+using CalculaJurosAPI.Controllers;
+
+namespace Tests.Unit.CalculaJurosAPI.Controllers
+{
+    public class ShowMeTheCodeControllerTest
+    {
+        [Fact]
+        public void ShowMeTheCode_DeveRetornarURLdoRepositorioDoProjeto()
+        {
+            // Arrange
+            var controller = new ShowMeTheCodeController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var githubRepo = "https://github.com/hrmsautran/code-challenge";
+            result.Should().Be(githubRepo);
+        }
+    }
+}
