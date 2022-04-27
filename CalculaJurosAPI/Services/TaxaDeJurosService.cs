8530d0ba6f13e98340f5a773ba7f245fbb9bdd09
Hermes Autran <hermes.autran@gmail.com>
2022-04-27 15:23:17 -0400

    Refact: add log

--- CalculaJurosAPI/Services/TaxaDeJurosService.cs
+++ CalculaJurosAPI/Services/TaxaDeJurosService.cs
@@ -5,25 +5,36 @@ namespace CalculaJurosAPI.Services
     public class TaxaDeJurosService : ITaxaDeJurosService
     {
         readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger _logger;
 
-        public TaxaDeJurosService(IHttpClientFactory httpClientFactory)
+        public TaxaDeJurosService(IHttpClientFactory httpClientFactory, ILogger<TaxaDeJurosService> logger)
         {
             _httpClientFactory = httpClientFactory;
+            _logger = logger;
         }
 
         public async Task<double> GetAsync()
         {
             using var httpClient = _httpClientFactory.CreateClient("apiTaxaDeJuros");
 
-            var result = await httpClient.GetAsync("taxaJuros");
-
-            if (!result.IsSuccessStatusCode)
+            try
             {
-                throw new HttpRequestException("Não foi possível buscar a taxa de juros.");
-            }
+                var result = await httpClient.GetAsync("taxaJuros");
 
-            var content = await result.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<double>(content);
+                if (!result.IsSuccessStatusCode)
+                {
+                    _logger.LogWarning("Erro ao recuperar taxa de juros.");
+                    throw new HttpRequestException("Não foi possível calcular os juros, favor tentar novamente.");
+                }
+
+                var content = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<double>(content);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Erro ao se conectar a API taxa de juros! Error: {ex.Message}");
+                throw new Exception("Não foi possível calcular os juros, favor tentar novamente.");
+            }
         }
     }
 }
