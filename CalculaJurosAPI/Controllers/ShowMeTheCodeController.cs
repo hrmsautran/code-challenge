4bc16b4ce5be98a3d2fb8f59c1941cf986f08185
Hermes Autran <hermes.autran@gmail.com>
2022-04-27 16:08:48 -0400

    Implemented ShowMeTheCode controller

---
+++ CalculaJurosAPI/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculaJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        private readonly string _githubRepository;
+
+        public ShowMeTheCodeController()
+        {
+            _githubRepository = "https://github.com/hrmsautran/code-challenge";
+        }
+
+        public string Get()
+        {
+            return this._githubRepository;
+        }
+    }
+}
